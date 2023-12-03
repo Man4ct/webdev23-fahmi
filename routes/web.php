bfd7e35aaa728f06d103a5f02709ee5fadb56ddc
fahmi.fahreza <fahmiiireza@gmail.com>
2023-12-03 11:09:27 +0100

    added routes to create and list forms

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\HomeController::class,
+'index'])->name('home')
+;
+Route::controller(\App\Http\Controllers\ArticleController::class)->group(function()
+{
+Route::get('/articles', 'list')->name('article.list');
+Route::match(['get', 'post'], '/articles/create',
+'create')->name('article.create');
 });
